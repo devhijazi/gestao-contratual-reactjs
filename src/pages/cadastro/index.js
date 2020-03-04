1b15edac5f5fdf55d92c9a0f7c71192a7f8222ca
Gabriel "Dev HijaZi" F <46979459+devhijazi@users.noreply.github.com>
2020-03-03 23:31:35 -0300

    update index.js

--- src/pages/cadastro/index.js
+++ src/pages/cadastro/index.js
@@ -49,7 +49,7 @@ const RegisterPage = ({ history }) => {
           <Row>
             <input
               className="input is-danger"
-              type="multiline"
+              multiline
               placeholder="Breve descrição"
               name="description"
             />
