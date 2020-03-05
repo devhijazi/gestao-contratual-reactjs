288c1f77be0666296bb3c3ad5d6c2a34d6b52f51
AvengerSuicide <dev.avenger.suicide@gmail.com>
2020-03-05 16:02:53 -0300

    fix api post bugs, and toast worked

--- src/pages/cadastro/index.js
+++ src/pages/cadastro/index.js
@@ -1,50 +1,69 @@
 import React from "react";
-import { Container, FormContainer, Row, Column, FormButton } from "./styles";
+import { toast } from "react-toastify";
 import { Form, Input } from "@rocketseat/unform";
 
 import api from "../../services/api";
 import { token } from "../../services/auth";
-import { toast } from "react-toastify";
 
-const RegisterPage = ({ history }) => {
+import {
+  Container,
+  Hero,
+  FormContainer,
+  Row,
+  Column,
+  FormButton
+} from "./styles";
+
+const RegisterPage = () => {
   async function handleSubmit(data) {
+    console.log(data);
     try {
-     await api.post("/contract", data, {
+      await api.post("/contract", data, {
         headers: { Authorization: token() }
       });
-    } catch (e) {
-      const error = e.response.data.error || "Ocorreu um erro";
 
+      const alerted = window.confirm("Cadastro salvo com sucesso!");
+      if (alerted) window.location.reload()
+    } catch (e) {
+      const response = e.response;
+      const error = (response && response.data.error) || "Ocorreu um erro";
       toast.error(error || "Verifique todos os campos");
     }
   }
 
   return (
     <Container>
+      <Hero className="hero">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title">Contrato</h1>
+            <h2 className="subtitle">Formulário de contrato</h2>
+          </div>
+        </div>
+      </Hero>
       <Form onSubmit={handleSubmit}>
         <FormContainer>
-          <h1>CADASTRO DE CONTRATO</h1>
           <Row>
             <label>Nome:</label>
-            <Input className="input" type="text" name="name" />
+            <Input className="input" type="text" name="name" required />
           </Row>
           <Row>
             <label>Data do inicio do contrato:</label>
-            <Input className="input" type="date" name="createdAt" />
+            <Input className="input" type="date" name="createdAt" required />
             <label>Data final do contrato:</label>
-            <Input className="input" type="date" name="finalAt" />
+            <Input className="input" type="date" name="finalAt" required />
           </Row>
           <Row>
             <label>Email para contato:</label>
-            <Input className="input" type="email" name="email" />
+            <Input className="input" type="email" name="email" required />
           </Row>
           <Row>
-            <label>Telefone(Opicional):</label>
+            <label>Telefone (Opicional):</label>
             <Input className="input" type="tel" name="contact" />
           </Row>
           <Row>
             <label>Adicionar descrição:</label>
-            <Input multiline className="input" name="description" />
+            <Input multiline className="textarea" name="description" required />
           </Row>
           <FormButton type="submit">SALVAR</FormButton>
           <Column />
