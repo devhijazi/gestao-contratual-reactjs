ccb36ef366c8e43c051458958a3dc639f080be82
Gabriel "Dev HijaZi" F <46979459+devhijazi@users.noreply.github.com>
2020-03-05 19:10:35 -0300

    update index

--- src/pages/lista/index.js
+++ src/pages/lista/index.js
@@ -1,11 +1,53 @@
 import React from "react";
 
-import { Container } from "./styles";
+import { Container, Row, RowHeader, RowItems } from "./styles";
 
 const listPage = ({ history }) => {
+  const items = [
+    { title: "Titulo", property: "name" },
+    { title: "Email", property: "email" },
+    { title: "Data Inicial", property: "createdAt" },
+    { title: "Descrição", property: "description" },
+    { title: "Data Final", property: "finalAt" }
+  ];
+
+  const data = [
+    {
+      name: "Nepto",
+      createdAt: "Ontem",
+      finalAt: "2021",
+      email: "abcde@x.com",
+      description: "Top mane"
+    },
+    {
+      name: "Nepto 2",
+      createdAt: "Ontem",
+      finalAt: "2021",
+      email: "abcde@x.com",
+      description: "Top mane"
+    },
+    {
+      name: "Nepto 3",
+      createdAt: "Ontem",
+      finalAt: "2021",
+      email: "abcde@x.com",
+      description: "Top mane"
+    }
+  ];
   return (
     <Container>
-      <p>Lista</p>
+      {items.map(item => (
+        <Row>
+          <RowHeader>
+            <h6>{item.title}</h6>
+          </RowHeader>
+          <RowItems>
+            {data.map(d => (
+              <p>{data[item.property]}</p>
+            ))}
+          </RowItems>
+        </Row>
+      ))}
     </Container>
   );
 };
--- src/pages/lista/styles.js
+++ src/pages/lista/styles.js
@@ -1,9 +1,14 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  display: flex;
+  display: block;
   width: 100%;
-  height: 100vh;
   justify-content: center;
   box-sizing: border-box;
 `;
+
+export const RowHeader = styled.header``;
+
+export const RowItems = styled.div``;
+
+export const Row = styled.div``;
