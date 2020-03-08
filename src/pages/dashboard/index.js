6bde6426e1553bf84cdf15d85def8f5548fd50a0
Gabriel "Dev HijaZi" F <46979459+devhijazi@users.noreply.github.com>
2020-03-07 22:31:46 -0300

    Dashboard style

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,17 +1,23 @@
 import React from "react";
-import { Container,Hero } from "./styles";
+import { Container, Hero, CounterContainer } from "./styles";
 
 const dashBoardPage = () => {
   return (
     <Container>
-          <Hero className="hero">
+      <Hero className="hero">
         <div className="hero-body">
           <div className="container">
             <h1 className="title">Painel de controle</h1>
-            <h2 className="subtitle">Contém informações básicas sobre o sistema.</h2>
+            <h2 className="subtitle">
+              Contém informações básicas sobre o sistema.
+            </h2>
           </div>
         </div>
       </Hero>
+      <CounterContainer>
+        <h2>Contratos cadastrados</h2>
+        <h1>0</h1>
+      </CounterContainer>
     </Container>
   );
 };
--- src/pages/dashboard/styles.js
+++ src/pages/dashboard/styles.js
@@ -4,8 +4,7 @@ export const Container = styled.div`
   display: block;
   width: 100%;
   justify-content: center;
-
-  
+  margin-bottom: 11%;
 `;
 export const Hero = styled.section`
   background-color: #bc96e6;
@@ -16,3 +15,24 @@ export const Hero = styled.section`
   }
 `;
 
+export const CounterContainer = styled.div`
+  justify-content: center;
+  padding: 60px 50px;
+  box-shadow: 0px 1px 2px 2px rgba(0, 0, 0, 0.3);
+  margin: 10px 10px;
+  border-radius: 5px;
+  h2 {
+    text-align: center;
+    font-style: oblique;
+    font-weight: bold;
+    font-size: 30px;
+    color: #6A5ACD;
+  }
+
+  h1 {
+    text-align: center;
+    font-weight: bold;
+    font-size: 50px;
+    color: #483D8B;
+  }
+`;
